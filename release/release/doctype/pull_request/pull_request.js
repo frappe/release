bb89743dfdd423ad505843cdeb262f3a6bef3f55
Suraj Shetty <surajshetty3416@gmail.com>
2021-02-24 11:10:11 +0530

    feat: Fetch PR description

--- release/release/doctype/pull_request/pull_request.js
+++ release/release/doctype/pull_request/pull_request.js
@@ -2,6 +2,26 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Pull Request", {
+	onload(frm) {
+		if (!frm.doc.pull_request_description && frm.doc.pull_request_link && frm.doc.docstatus === 0) {
+			let pr_data = frm.doc.pull_request_link.split('/')
+
+			let pr_number = pr_data[pr_data.length - 1];
+			let pr_repo = pr_data[pr_data.length - 3];
+			let pr_org = pr_data[pr_data.length - 4];
+			if (frm.doc.pull_request_title.includes("(bp #")) {
+				pr_number = frm.doc.pull_request_title.match(/#(\d+)/)[1]
+			}
+			if (pr_number && pr_repo && pr_org) {
+				fetch(`https://api.github.com/repos/${pr_org}/${pr_repo}/pulls/${pr_number}`)
+				.then(response => response.json())
+				.then(data => {
+					frm.set_value('pull_request_description', data.body || 'No description found!');
+					frm.save()
+				});
+			}
+		}
+	},
 	refresh: function(frm) {
 		if (frm.doc.pull_request_link) {
 			frm.add_custom_button("Open PR", () =>
